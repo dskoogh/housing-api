721ca8c37300a6d5946807b38cb8c928a2bac68e
dafvidskoogh <dafvid.skoogh@gmail.com>
2020-04-13 11:32:39 +0200

    Haaands

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => res.send("Hello world!"));
-app.listen(port, () =>
-  console.log(`Example app listening at http://localhost:${port}`)
-);
+console.log("App running!");
+// app.listen(port, () =>
+//   console.log(`Example app listening at http://localhost:${port}`)
+// );
